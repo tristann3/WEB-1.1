3583ec6f45b8f81c443ea0f1d475811016c499da
Tristan <tristan15thompson@gmail.com>
2020-12-07 15:35:53 -0800

    updated dependencies

--- hwk5-Databases/app.py
+++ hwk5-Databases/app.py
@@ -55,6 +55,7 @@ def detail(plant_id):
     # TODO: Replace the following line with a database call to retrieve *one*
     # plant from the database, whose id matches the id passed in via the URL.
     plant_to_show = mongo.db.plants.find({ '_id': ObjectId(plant_id)})
+    print(plant_to_show)
 
 
     # TODO: Use the `find` database operation to find all harvests for the
