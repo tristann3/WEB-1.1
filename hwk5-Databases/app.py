f0e4d5c343a29d98698f5d44007253feee785892
Tristan <tristan15thompson@gmail.com>
2020-12-08 11:30:10 -0800

    [FIX] fixed delete route and cleaned code

--- hwk5-Databases/app.py
+++ hwk5-Databases/app.py
@@ -54,7 +54,7 @@ def detail(plant_id):
 
     # TODO: Replace the following line with a database call to retrieve *one*
     # plant from the database, whose id matches the id passed in via the URL.
-    plant_to_show = mongo.db.plants.find({ '_id': ObjectId(plant_id)})
+    plant_to_show = mongo.db.plants.find_one({ '_id': ObjectId(plant_id)})
     print(plant_to_show)
 
 
